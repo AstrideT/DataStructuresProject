7d3c887e789bc33e410c0abdcfb1b1411dbbeff9
AstrideT <j00790204@students.jsums.edu>
2017-10-01 04:09:35 -0500

    Update Dynamic List Array:Adding member functions sublist,rotateList,reverseList and segregateevenodd to a dynamic list array.cpp

--- Dynamic List Array:Adding member functions sublist,rotateList,reverseList and segregateevenodd to a dynamic list array.cpp
+++ Dynamic List Array:Adding member functions sublist,rotateList,reverseList and segregateevenodd to a dynamic list array.cpp
@@ -1,657 +1,317 @@
-
-
 //****************************************************************************
-
-//Project
-1: Implementing the dynamic List ADT using array
-
-//Creating
-member functions: print,sublist,rotatelist, reverse list and segregate even odd
-
+//Project 1: Implementing the dynamic List ADT using array
+//Creating member functions: print,sublist,rotatelist, reverse list and segregate even odd
 //**********************************************************************
 
- 
-
 #include <iostream>
-
 #include <algorithm>
-
 using namespace std;
 
- 
-
- 
-
- 
-
+　
+　
 class List
 
- 
-
 {
-
- 
 
 private:
+	int *array;
+	int maxSize;         // useful to decide if resizing (doubling the array size) is needed
+	int endOfArray;     //The last index of array lengthofArray-1
 
-int *array;
-
-int maxSize;         // useful to decide if resizing (doubling the array size)
-is needed
-
-int endOfArray;     //The last index of array lengthofArray-1
-
- 
-
- 
-
- 
-
+　
+　
 public:
+	List(int size)
 
-List(int size)
+	{
+		array = new int[size];
+		maxSize = size;
+		endOfArray = -1;
 
- 
+	}
 
-       {
+　
+	bool isEmpty()
 
-              array = new int[size];
+	{
+		if (endOfArray == -1)
+			return true;
+		return false;
 
-              maxSize = size;
+	}
 
-              endOfArray = -1;
+	void resize(int s)
 
- 
+	{
+		int *tempArray = array;
+		array = new int[s];
 
-       }
+		for (int index = 0; index < min(s, endOfArray + 1); index++)
+		{
+			array[index] = tempArray[index];
+		}
+		maxSize = s;
 
- 
+	}
 
- 
+　
+　
+	void insert(int data)//inserts data at the end of array
 
-bool isEmpty()
+	{
+		if (endOfArray == maxSize - 1)
+			resize(2 * maxSize);
 
- 
+		array[++endOfArray] = data;
 
-       {
+	}
 
-       if (endOfArray == -1)
+　
+	void insertAtIndex(int insertIndex, int data)
 
-              return true;
+	{
 
-       return false;
+		// if the user enters an invalid insertIndex, the element is
 
- 
+		// appended to the array, after the current last element
 
-       }
+		if (insertIndex > endOfArray + 1)
 
- 
+			insertIndex = endOfArray + 1;
 
-void resize(int s)
+　
+　
+		if (endOfArray == maxSize - 1)
 
- 
+			resize(2 * maxSize);
 
-       {
+　
+　
+		for (int index = endOfArray; index >= insertIndex; index--)
 
-       int *tempArray = array;
+			array[index + 1] = array[index];
 
-       array = new
-int[s];
+		array[insertIndex] = data;
 
- 
+		endOfArray++;
 
-       for (int index = 0; index < min(s, endOfArray + 1); index++)
+	}
 
-              {
+　
+　
+	int read(int index)
 
-                     array[index] =
-tempArray[index];
+	{
 
-       }
+		return array[index];
 
-       maxSize = s;
+	}
 
- 
+　
+　
+	void modifyElement(int index, int data)
 
-       }
+	{
 
- 
+		array[index] = data;
 
- 
+	}
 
- 
+　
+　
+	void deleteElement(int deleteIndex)
 
-       void insert(int data)//inserts data at the end of
-array
+	{
 
- 
+		/* shift elements one cell to the left starting from deleteIndex+1 to endOfArray-1
 
-       {
+		i.e., move element at deleteIndex+1 to deleteIndex and so on */
 
-              if (endOfArray == maxSize - 1)
+		for (int index = deleteIndex; index < endOfArray; index++)
 
-                     resize(2 * maxSize);
+			array[index] = array[index + 1];
 
- 
+		endOfArray--;
 
-              array[++endOfArray] = data;
+	}
 
- 
+　
+　
+	int countList()
 
-       }
+	{
 
- 
+		int count = 0;
 
- 
+		for (int index = 0; index <= endOfArray; index++)
 
-       void insertAtIndex(int insertIndex, int data)
+			count++;
 
- 
+		return count;
 
-       {
+	}
 
- 
+　
+	//New added member functions 
+	void print()
 
-              //
-if the user enters an invalid insertIndex, the element is
+	{
 
- 
+		for (int index = 0; index <= endOfArray; index++)
 
-              //
-appended to the array, after the current last element
+			cout << array[index] << " ";
+		cout << endl;
 
- 
+	}
 
-              if (insertIndex > endOfArray + 1)
+	List subList(int fromIndex, int toIndex) {
 
- 
+		int length = (toIndex - fromIndex) + 1;
+		List* subarr = new List(length);
 
-                     insertIndex = endOfArray + 1;
+　
+		for (int i = 0; i <length; i++) {
+			subarr->insertAtIndex(i, array[fromIndex]);
+			fromIndex++;
 
- 
+		}
+		return *subarr;
+	}
 
- 
+	void rotateList(int rotationSpace) {
 
- 
+		int temp;
 
-              if (endOfArray == maxSize - 1)
+		for (int i = 0; i < rotationSpace; i++) {
 
- 
+			temp = array[0];
 
-                     resize(2 * maxSize);
+			for (int j = 0; j < endOfArray; j++) {
 
- 
+				array[j] = array[j + 1];
 
- 
+			}
 
- 
+			array[endOfArray] = temp;
 
-              for (int index = endOfArray; index >= insertIndex; index--)
+		}
 
- 
+	}
 
-                     array[index + 1] =
-array[index];
+	void reverseList() {
 
- 
+　
+		for (int i = 0; i < (endOfArray + 1) / 2; i++) {
 
-              array[insertIndex] = data;
+			int temp = array[i];
 
- 
+			array[i] = array[endOfArray - i];
 
-              endOfArray++;
+			array[endOfArray - i] = temp;
 
- 
+		}
 
-       }
+	};
 
- 
+	void segregateEvenOdd() {
+		int temp;
+		int endPoint = endOfArray;
 
- 
+　
+		for (int i = 0; i <= endPoint; i++) {
 
- 
+			if ((array[i] % 2) == 0) {
 
-       int read(int index)
+				array[i] = array[i];
+			}
+			else
+			{
+				temp = array[i];
+				for (int index = i; index < endPoint; index++)
+					array[index] = array[index + 1];
 
- 
+				array[endPoint] = temp;
+				if (array[i] % 2 != 0)
+				{
+					i--;
+					endPoint--;
 
-       {
+				}
 
- 
+　
+　
+			}
+		}
 
-              return array[index];
+	}
 
- 
-
-       }
-
- 
-
- 
-
- 
-
-       void modifyElement(int index, int data)
-
- 
-
-       {
-
- 
-
-              array[index] = data;
-
- 
-
-       }
-
- 
-
- 
-
- 
-
-       void deleteElement(int deleteIndex)
-
- 
-
-       {
-
- 
-
-              /*
-shift elements one cell to the left starting from deleteIndex+1 to endOfArray-1
-
- 
-
-              i.e., move element at
-deleteIndex+1 to deleteIndex and so on */
-
- 
-
-              for (int index = deleteIndex; index < endOfArray; index++)
-
- 
-
-                     array[index] = array[index
-+ 1];
-
- 
-
-              endOfArray--;
-
- 
-
-       }
-
- 
-
- 
-
- 
-
-       int countList()
-
- 
-
-       {
-
- 
-
-              int count = 0;
-
- 
-
-              for (int index = 0; index <= endOfArray; index++)
-
- 
-
-                     count++;
-
- 
-
-              return count;
-
- 
-
-       }
-
- 
-
- 
-
-//New
-added member functions 
-
-       void print()
-
- 
-
-       {
-
- 
-
-              for (int index = 0; index <= endOfArray; index++)
-
- 
-
-                     cout << array[index] << " ";
-
-              cout << endl;
-
- 
-
-       }
-
- 
-
-       List subList(int fromIndex, int toIndex) {
-
- 
-
-              int length = (toIndex - fromIndex)+1;
-
-              List* subarr = new List(length);
-
- 
-
- 
-
-              for (int i = 0; i <length; i++) {
-
-                     subarr->insertAtIndex(i,
-array[fromIndex]);
-
-                     fromIndex++;
-
- 
-
-              }
-
-              return *subarr;
-
-       }
-
- 
-
-       void rotateList(int rotationSpace) {
-
- 
-
-              int temp;
-
- 
-
-              for (int i = 0; i < rotationSpace; i++) {
-
- 
-
-                     temp = array[0];
-
- 
-
-                     for (int j = 0; j < endOfArray;
-j++) {
-
- 
-
-                           array[j] = array[j +
-1];
-
- 
-
-                     }
-
- 
-
-                     array[endOfArray] = temp;
-
- 
-
-              }
-
- 
-
-       }
-
- 
-
-       void reverseList() {
-
-              
-
- 
-
-              for (int i = 0; i < (endOfArray+1 )/ 2; i++) {
-
- 
-
-                     int temp = array[i];
-
- 
-
-                     array[i] =
-array[endOfArray-i];
-
- 
-
-                     array[endOfArray - i] =
-temp;
-
- 
-
-              }
-
- 
-
-       };
-
- 
-
-       void segregateEvenOdd() {
-
-              int temp ;
-
-              int endPoint = endOfArray;
-
- 
-
- 
-
-              for (int i = 0; i <= endPoint; i++) {
-
- 
-
-                     if ((array[i] % 2) == 0) {
-
- 
-
-                           array[i] = array[i];
-
-                     }
-
-                     else
-
-                     {
-
-                           temp = array[i];
-
-                           for (int index = i; index <
-endPoint; index++)
-
-                                  array[index] =
-array[index + 1];
-
- 
-
-                           array[endPoint] =
-temp;
-
-                           if (array[i] % 2 != 0)
-
-                           {
-
-                                  i--;
-
-                                  endPoint--;
-
- 
-
-                           }
-
- 
-
- 
-
- 
-
-                     }
-
-              }
-
-       
-
-       }
-
-       
-
- 
-
- 
-
- 
-
+　
+　
+　
 };
 
- 
-
- 
-
- 
-
- 
-
+　
+　
+　
 int main()
-
- 
 
 {
 
- 
+	int listSize;
 
-       int listSize;
+	cout << "Enter list size: ";
 
- 
+	cin >> listSize;
 
-       cout << "Enter list size:
-";
+　
+　
+	List integerList(1); // we will set the maxSize to 1 and double it as and when needed
 
- 
+　
+　
+	for (int i = 0; i < listSize; i++)
 
-       cin >> listSize;
+	{
 
- 
+		int value;
 
- 
+		cout << "Enter element # " << i << " : ";
 
- 
+		cin >> value;
 
-       List integerList(1); // we
-will set the maxSize to 1 and double it as and when needed
+　
+　
+		integerList.insertAtIndex(i, value);
 
-       
+	}
 
- 
+　
+　
+	cout << "Original List: ";
 
- 
+	integerList.print();
 
-       for (int i = 0; i < listSize; i++)
+	List sublist = integerList.subList(2, 4);
+	cout << "Sub list from 2 to 4: ";
+	sublist.print();
 
- 
+	integerList.rotateList(2);
+	cout << "After rotation(2 elements to the left):";
+	integerList.print();
 
-       {
+	integerList.reverseList();
+	cout << "After reverse: ";
+	integerList.print();
 
- 
+　
+　
+	integerList.segregateEvenOdd();
+	cout << "After even or odd segregation: ";
 
-              int value;
+	integerList.print();
 
- 
+	return 0;
+}
 
-              cout << "Enter element # " << i << " : ";
-
- 
-
-              cin >> value;
-
- 
-
- 
-
- 
-
-              integerList.insertAtIndex(i,
-value);
-
- 
-
-       }
-
- 
-
- 
-
- 
-
-       cout << "Original List: ";
-
- 
-
-       integerList.print();
-
- 
-
-       List sublist= integerList.subList(2, 4);
-
-       cout << "Sub list from 2 to 4:
-";
-
-       sublist.print();
-
-       
-
-       integerList.rotateList(2);
-
-       cout << "After rotation(2
-elements to the left):";
-
-       integerList.print();
-
- 
-
-       integerList.reverseList();
-
-       cout << "After reverse: ";
-
-       integerList.print();
-
-       
-
-       
-
- 
-
-       integerList.segregateEvenOdd();
-
-       cout << "After even or odd
-segregation: ";
-
- 
-
-       integerList.print();
-
-       
-
-       return 0;
-
-       }
-
+　
+　
